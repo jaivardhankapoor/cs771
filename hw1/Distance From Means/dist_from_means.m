643730c13e7f55fec6e6fad748fa7196491c4588
Jaivardhan Kapoor <jaivardhan.kapoor@gmail.com>
2016-08-31 23:56:04 +0530

    done hw1

--- hw1/Distance From Means/dist_from_means.m
+++ hw1/Distance From Means/dist_from_means.m
@@ -1,12 +1,14 @@
 function dist_from_means()
     load('mnist_hw1.mat');
     acc_arr = [];
-    for iter = 1:10
+    for iter = 1:30
+        fprintf('Iteration %2.0f',iter);
         acc_arr_temp = [];
         for k=50:50:2000
             acc_arr_temp = [acc_arr_temp,predict(dataX, X_test, Y_test, k)];
         end
         acc_arr = [acc_arr;acc_arr_temp];
+        fprintf('\b\b\b\b\b\b\b\b\b\b\b\b');
     end
     acc_arr = mean(acc_arr);
     plot(50:50:2000,acc_arr),xlabel('Number of points used to calculate mean'),ylabel('Test accuracy'),title('Distance from means - accuracy vs mean sampling set size');
